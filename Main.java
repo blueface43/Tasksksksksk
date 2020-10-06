1698417930227c60b73b1f8df00c5a2938474b5b
blueface43 <63073069+blueface43@users.noreply.github.com>
2020-10-06 16:46:05 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
 package com.company;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner=new Scanner(System.in);
+        float a,b,c,z;
+        System.out.println("Enter a,b,c: ");
+        a= scanner.nextFloat();
+        b= scanner.nextFloat();
+        c= scanner.nextFloat();
+        z=((a-3)*b/2)+c;
+        System.out.println("Value z="+z);
     }
 }
---
+++ task3/task3.java
@@ -0,0 +1,7 @@
+package com.company.task3;
+
+public class task3 {
+    public static void main(String[] args) {
+
+    }
+}
